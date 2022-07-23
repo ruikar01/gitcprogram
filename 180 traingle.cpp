523fdb6526a3a77f2bbff82d23dcd39d0225f82c
ruikar01 <109852264+ruikar01@users.noreply.github.com>
2022-07-23 15:56:22 +0530

    first commit

---
+++ 180 traingle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int angle1,angle2,angle3,sum;
+	cout<<"input the three angle of traingle";
+	cin>>angle1>>angle2>>angle3;
+	sum=angle1+angle2+angle3;
+	if(sum==180)
+	{
+		cout<<"the traingle is valid";
+	}
+	else
+	{
+		cout<<"the traingle is invalid";
+	}
+	
+}
---
+++ project/2d lower.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+main()
+{
+	int a[3][3],i,j;
+	printf("\n enter the matrix");
+	for(i=0;i<=2;i++)
+	{
+		for(j=0;j<=2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<=2;i++)
+	{
+		for(j=0;j<=2;j++)
+		{
+			if(i<j)
+			{
+				a[i][j]=0;
+			}
+		}
+	}
+	for(i=0;i<=2;i++)
+	{
+		for(j=0;j<=2;j++)
+		{
+			printf("\t%d",a[i][j]);
+		}
+		printf("\n");
+	}
+}
